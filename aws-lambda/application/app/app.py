cef6aa08f37dc565cc61a02ba9dd679ba4c68e35
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2021-11-01 13:56:26 -0300

    SQS demo

--- aws-lambda/application/app/app.py
+++ aws-lambda/application/app/app.py
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
     print("S3 - config content:", json_content)
     
     # SQS
-    print("### SQS")
+    print("### SQS DEMO")
     sqs = get_sqs_client()
     # Enviando evento
     response = sqs.send_message(QueueUrl=SQS_QUEUE_URL, MessageBody='world')
